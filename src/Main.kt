aafcb87b28cb5c674f99042723837370a13509ef
Caramelo <cccarlos@iesrafaelalberti.es>
2025-01-28 20:02:15 +0100

    mejoras de código

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,33 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("HelloSSWS  , " + name + "!")
-
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+class Cuenta(var numCuenta : Int, var saldo : Double){
+    fun esMorosa(): Boolean{
+        if (saldo <= 0){
+            return true
+        }
+        return false
     }
+    fun transferirDinero(persona1: Persona, persona2: Persona, cuenta1: Cuenta,cuenta2: Cuenta,saldo: Double) : Boolean{
+        if ((saldo + cuenta1.saldo >= 0) && (cuenta2.saldo - saldo >= 0)){
+            return true
+        }
+        return false
+    }
+}
+
+class Persona( var dni : String, cuentas : Array<Cuenta>){
+    private val maxCuentas : Int = 3
+    init {
+        require(dni.length == 9){"El DNI debe ser de 9 digitos"}
+        require(cuentas.size <= maxCuentas){"No puedes tener más de 3 cuentas"}
+        }
+
+    fun añadirCuenta(nuevaCuenta : Cuenta){
+
+    }
+
+}
+
+
+
+fun main() {
+
 }
